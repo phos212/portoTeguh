fa3c5697f0d012f8f8548bac69a38247ad5a8f55
Teguh Arif Hidayatuloh <teguhrollie@gmail.com>
2026-09-23 09:47:41 +0700

    Restore cv.html without download PDF feature, restore Download CV button in index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,4 @@
-﻿function downloadPDF() {
-    const element = document.body;
-    const opt = {
-        margin: 10,
-        filename: 'Teguh_Arif_Hidayatuloh_CV.pdf',
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2 },
-        jsPDF: { orientation: 'portrait', unit: 'mm', format: 'a4' }
-    };
-    html2pdf().set(opt).from(element).save();
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+﻿document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('a[href^="#"]');
     links.forEach(link => {
         link.addEventListener('click', function(e) {
